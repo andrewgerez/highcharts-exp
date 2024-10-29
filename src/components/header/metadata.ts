ffe69caf79d02ce93b6e85a293e52a3fa80052dd
andrewgerez <andrewgerez18@gmail.com>
2024-10-29 03:10:42 -0300

    feat: create header metadata

---
+++ src/components/header/metadata.ts
@@ -0,0 +1,28 @@
+import { PresetDataProps } from './types'
+
+export const presetDatas: PresetDataProps[] = [
+  {
+    title: 'Yesterday',
+    value: 'yesterday'
+  },
+  {
+    title: 'Last Week',
+    value: 'last_week'
+  },
+  {
+    title: 'Last 30 Days',
+    value: 'last_30_days'
+  },
+  {
+    title: 'Last 3 Months',
+    value: 'last_3_months'
+  },
+  {
+    title: 'Last 6 Months',
+    value: 'last_6_months'
+  },
+  {
+    title: 'Last 12 Months',
+    value: 'last_12_months'
+  },
+]
