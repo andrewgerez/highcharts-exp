72f4fed1ab2eec0497a39e36bc6c0296d382caf4
andrewgerez <andrewgerez18@gmail.com>
2024-10-29 03:11:25 -0300

    feat: create axios adapter

---
+++ src/infra/axios-adapter.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpRequest, HttpResponse } from '@/data/interfaces/http/http-client'
+import axios, { AxiosResponse } from 'axios'
+
+export class AxiosAdapter implements HttpClient {
+  async request(params: HttpRequest): Promise<HttpResponse<any>> {
+    let httpResponse: AxiosResponse<any>
+
+    try {
+      httpResponse = await axios.request({
+        url: params.url,
+        method: params.method,
+        data: params.body,
+        headers: params.headers
+      })
+    } catch (error: any) {
+      httpResponse = error.response
+    }
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    }
+  }
+}
