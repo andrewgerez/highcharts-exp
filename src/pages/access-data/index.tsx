a4d0bc567ee684323245a85bb0c712619d653c48
andrewgerez <andrewgerez18@gmail.com>
2024-10-30 02:47:31 -0300

    feat: imports

--- src/pages/access-data/index.tsx
+++ src/pages/access-data/index.tsx
@@ -1,7 +1,10 @@
 import Header from '@/components/header'
+import PercentageOfAccessOnPages from './_components/percentage-of-access-on-pages'
+import TotalAccessOnPages from './_components/total-access-on-pages'
+import TotalAccessPerHour from './_components/total-access-per-hour'
+import TotalAccessPerMonth from './_components/total-access-per-month'
 import { AnalyticsDataProvider } from '@/shared/contexts/analytics-data-context'
 import { Content } from './styles'
-import PercentageOfAccessOnPages from './_components/percentage-of-access-on-pages'
 
 function AccessData() {
   return (
@@ -9,7 +12,10 @@ function AccessData() {
       <Header />
 
       <Content>
+        <TotalAccessOnPages />
         <PercentageOfAccessOnPages />
+        <TotalAccessPerHour />
+        <TotalAccessPerMonth />
       </Content>
     </AnalyticsDataProvider>
   )
