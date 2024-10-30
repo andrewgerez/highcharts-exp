bcb0451e13061609d6e875b742df01f9d4085c9c
andrewgerez <andrewgerez18@gmail.com>
2024-10-30 13:12:54 -0300

    refactor: adjust palette

--- src/shared/constants/global.ts
+++ src/shared/constants/global.ts
@@ -2,7 +2,7 @@ export const AnalyticsColors = {
   black: "#191622",
   grey: "#d1ced3",
   lightGrey: "#F4F4F4",
-  ecWine: "#DD2919",
+  Wine: "#DD2919",
   purple: "#8257E5",
   darkGray: "#78787F",
 }
