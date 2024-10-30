54664ad6b03460adeae189c9fa3e93da83feed50
andrewgerez <andrewgerez18@gmail.com>
2024-10-30 02:38:16 -0300

    feat: create total access per month

---
+++ src/pages/access-data/_components/total-access-per-month/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { AnalyticsColors } from '@/shared/constants/global'
+
+export const Wrapper = styled.div`
+    grid-area: totalAccessPerHour;
+    border: 1px solid ${AnalyticsColors.grey};
+`
