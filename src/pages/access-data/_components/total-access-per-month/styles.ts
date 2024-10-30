49d837c758c2ff716f3fa7a18a21453a458ba2ea
andrewgerez <andrewgerez18@gmail.com>
2024-10-30 13:36:24 -0300

    refactor: adjust total access per month styles

--- src/pages/access-data/_components/total-access-per-month/styles.ts
+++ src/pages/access-data/_components/total-access-per-month/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 import { AnalyticsColors } from '@/shared/constants/global'
 
 export const Wrapper = styled.div`
-    grid-area: totalAccessPerHour;
+    grid-area: totalAccessPerMonth;
     border: 1px solid ${AnalyticsColors.grey};
 `
